a57e10b0e4fc3c0e53c6e1b6c3883bf305f2eb39
meblendi <mhdsaffari@gmail.com>
2025-08-27 15:47:30 +0330

    Initial commit

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,7 +3,7 @@ import { useAuth } from '../hooks/useAuth';
 import { useApi } from '../hooks/useApi';
 import { Order } from '../types';
 import Layout from '../app/layout';
-import OrderCard from '../components/OrderCard';
+import OrderCard from '../app/OrderCard';
 
 const Dashboard: React.FC = () => {
   const { user } = useAuth();
