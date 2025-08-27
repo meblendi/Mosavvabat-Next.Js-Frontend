76990494c2d248e83c88507de6edd4b0a1039283
meblendi <mhdsaffari@gmail.com>
2025-08-27 14:33:39 +0330

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/api/:path*',
+      },
+    ];
+  },
+  env: {
+    API_URL: process.env.API_URL || 'http://localhost:8000',
+  },
 };
 
 export default nextConfig;
