cc8239141a5a6af60035b9cc7a33033579ff2c96
meblendi <mhdsaffari@gmail.com>
2025-09-06 09:22:02 +0330

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,18 +9,16 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 
-// This is the main layout that gets exported
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="fa" dir="rtl">
       <body>
         <ClientWrapper>
-          <div className="min-h-screen bg-gray-50">
-            <Header />
-            <div className="flex">
-              <Sidebar />
-              <main className="flex-1 p-6">{children}</main>
-            </div>
+          {/* Header and Sidebar will conditionally render based on auth state */}
+          <Header />
+          <div className="flex">
+            <Sidebar />
+            <main className="flex-1 p-6">{children}</main>
           </div>
           <Toaster position="top-center" />
         </ClientWrapper>
